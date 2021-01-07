e1bf793632faa4b523d510a95651d90d47e034b8
makemek <maimake@hotmail.com>
2021-01-08 03:20:06 +0700

    use template react document instead of response string

--- src/server.js
+++ src/server.js
@@ -3,48 +3,26 @@ import React from 'react'
 import { StaticRouter } from 'react-router-dom'
 import express from 'express'
 import { renderToString } from 'react-dom/server'
+import Document from './Document'
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST)
-
 const server = express()
-server
-  .disable('x-powered-by')
-  .get('/*', (req, res) => {
-    const context = {}
-    const markup = renderToString(
+
+server.disable('x-powered-by').get('/*', (req, res) => {
+  const context = {}
+  const markup = renderToString(
+    <Document assets={assets.client}>
       <StaticRouter context={context} location={req.url}>
         <App />
-      </StaticRouter>,
-    )
+      </StaticRouter>
+    </Document>,
+  )
 
-    if (context.url) {
-      res.redirect(context.url)
-    } else {
-      res.status(200).send(
-        `<!doctype html>
-    <html lang="">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta charset="utf-8" />
-        <title>Welcome to Razzle</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${
-          assets.client.css
-            ? `<link rel="stylesheet" href="${assets.client.css}">`
-            : ''
-        }
-        ${
-          process.env.NODE_ENV === 'production'
-            ? `<script src="${assets.client.js}" defer></script>`
-            : `<script src="${assets.client.js}" defer crossorigin></script>`
-        }
-    </head>
-    <body>
-        <div id="root">${markup}</div>
-    </body>
-</html>`,
-      )
-    }
-  })
+  if (context.url) {
+    return res.redirect(context.url)
+  }
+  res.type('html')
+  return res.send(markup)
+})
 
 export default server
