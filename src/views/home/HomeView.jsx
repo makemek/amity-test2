93ec5c136ae2ca39d98c74090b4ec7da1d01fb93
makemek <maimake@hotmail.com>
2021-01-10 15:52:55 +0700

    ui

--- src/views/home/HomeView.jsx
+++ src/views/home/HomeView.jsx
@@ -1,5 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 export default function HomeView() {
-  return <div>Hello World</div>
+  const inputRef = useRef(null)
+
+  return (
+    <>
+      <section>
+        <h1>Input Route</h1>
+        <div>
+          <div>Routes </div>
+          <textarea ref={inputRef} />
+        </div>
+      </section>
+      <section>
+        <h1>Calculate delivery cost</h1>
+        <div>
+          <div>Route </div>
+          <input type="text" />
+        </div>
+        <button onClick={onCalculateDevlieryCost}>
+          Calculate
+        </button>
+      </section>
+    </>
+  )
+
+  function onCalculateDevlieryCost() {}
 }
