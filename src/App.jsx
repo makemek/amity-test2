a64a18b11bee7c1a9f019429cfbba05a666ebf9e
makemek <maimake@hotmail.com>
2021-01-10 12:39:45 +0700

    restructure folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Home from './Home'
+import { HomeView } from './views/home'
 
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 const App = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
+    <Route exact path="/" component={HomeView} />
   </Switch>
 )
 
